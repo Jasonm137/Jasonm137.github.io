a7ca913280c12089214fc8747627d5546e1f401c
Jasonm137 <76846496+Jasonm137@users.noreply.github.com>
2021-12-18 15:02:01 -0800

    Add files via upload
    hello

---
+++ Server/Index.js
@@ -0,0 +1,30 @@
+const express = require ("express");
+
+const app = express ();
+
+var participants = [0,0,0,0,0,0,0,0,0,0,0,0,0];
+var voters = {};
+
+app.get("/", function (req, res) {
+    //console.log(req.query);
+    var pid = req.query.id;
+    if (pid<1 || pid>12) {
+        return;
+    }
+
+    participants[pid]++;
+    console.log(participants);
+
+    if (!voters[pid]) {
+        voters[pid] = [];
+    }
+    voters[pid].push(req.query.email);
+        
+    
+    console.log(voters);
+    res.send("success");
+});
+
+app.listen(3000, function (){
+    console.log("server listening on port 3000!")
+});
